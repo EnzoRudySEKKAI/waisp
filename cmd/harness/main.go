51cec752cf092cf416ee9d8cb42d409eda1dd655
Enzo Rudy SEKKAÏ <37474838+EnzoRudySEKKAI@users.noreply.github.com>
2026-08-20 12:13:00 +0800

    Create harness runtime entrypoint

--- cmd/harness/main.go
+++ cmd/harness/main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("waïsp harness starting...")
+	fmt.Println("waïsp harness starting")
+	fmt.Println("DeepSeek harness adapter will be connected here")
 }
